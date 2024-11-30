18c47589d474dd65f9dde710234b59a6f20d32f4
nitesh chaturvedi <nitesh@modulobytes.com>
2024-11-30 17:59:36 +0530

    user Profile in App using default import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import MyComponent from './components/first/MyComponent'
 // import Gallery from './components/gallery/Gallery';
-import { Profile } from './components/gallery/Gallery';
+// import { Profile } from './components/gallery/Gallery';
+import Profile from './components/profile/profile';
 
 function App() {
   return (
