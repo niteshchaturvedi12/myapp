2a9257436e2ca9f801217d5999d429ba770c4bd6
nitesh chaturvedi <nitesh@modulobytes.com>
2024-11-30 18:00:30 +0530

    using Gallery as default import in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import MyComponent from './components/first/MyComponent'
-// import Gallery from './components/gallery/Gallery';
+import Gallery from './components/gallery/Gallery';
 // import { Profile } from './components/gallery/Gallery';
-import Profile from './components/profile/profile';
+// import Profile from './components/profile/profile';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
-      {/* <Gallery /> */}
-      <Profile />
+      <Gallery />
     </div>
   );
 }
