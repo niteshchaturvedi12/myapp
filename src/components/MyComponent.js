d0fb088f21ea6890caa37a7c4cd68b2811fa8b78
nitesh chaturvedi <nitesh@modulobytes.com>
2024-11-30 16:16:44 +0530

    my first component is ready

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MyComponent.css'
+
+export default function MyComponent() {
+  return (
+    <div className='container mc'>
+      <article>
+        <h1>My First Componenet</h1>
+        <ol>
+          <li>Component: UI Building Blocks</li>
+          <li>Defining a Component</li>
+          <li>Using a Component</li>
+        </ol>
+      </article>
+    </div>
+  )
+}
