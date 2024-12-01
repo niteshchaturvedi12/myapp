30f74f7f3861021dd871624f00e4d7e6a104feb2
nitesh chaturvedi <nitesh@modulobytes.com>
2024-12-01 22:02:17 +0530

    changes to todo component

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import './ToDo.css'
 
+const today = new Date()
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long'}
+  ).format(date);
+}
 export default function ToDo() {
+  const name = "Hedy Lamarr";
   return (
     <div className='container todo'>
-        <h1>Hedy Lamarr's Todos</h1>
-        <img src='https://i.imgur.com/yXOvdOSs.jpg' alt='Hedy Lamarr' className='photo'/>
+        <h1>{name}'s Todos for {formatDate(today)}</h1>
+        <img src='https://i.imgur.com/yXOvdOSs.jpg' alt={name} className='photo'/>
         <ul>
             <li>Invent new traffic lights</li>
             <li>Rehearse a movie scene</li>
